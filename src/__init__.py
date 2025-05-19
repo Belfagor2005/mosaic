7995791e7d622dcfecee5621825bbe5f06f2e2c3
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2025-05-19 16:48:33 +0200

    Init update
    Fix: - screen - record widget - skin - refactoring all code py3

---
+++ src/__init__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+import os
+import gettext
+
+from Components.Language import language
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS
+
+PluginLanguageDomain = "Mosaic"
+PluginLanguagePath = "Extensions/Mosaic/locale"
+
+isDreambox = os.path.exists("/usr/bin/apt-get")
+
+
+def localeInit():
+	if isDreambox:
+		lang = language.getLanguage()[:2]
+		os.environ["LANGUAGE"] = lang
+	if PluginLanguageDomain and PluginLanguagePath:
+		gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
+
+
+if isDreambox:
+	def _(txt):
+		return gettext.dgettext(PluginLanguageDomain, txt) if txt else ""
+else:
+	def _(txt):
+		translated = gettext.dgettext(PluginLanguageDomain, txt)
+		if translated:
+			return translated
+		else:
+			print("[%s] fallback to default translation for %s" % (PluginLanguageDomain, txt))
+			return gettext.gettext(txt)
+
+localeInit()
+language.addCallback(localeInit)
